99b6fb691bc3d49061cb07abd08947ae6c5159d2
stockpricefyp <75793312+sooraj671@users.noreply.github.com>
2024-09-15 23:30:46 +0500

    get profile endpoint fixed

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -109,11 +109,11 @@ const login = async (req, res) => {
 
     const payload = {
       user: {
-        id: user.id,
+        id: user._id,
       },
     };
 
-    jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' }, (err, token) => {
+    jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '24h' }, (err, token) => {
       if (err) throw err;
       res.json({ token });
     });
